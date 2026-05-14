40f0b7fb716c6e79021b6da986bf4c62a254de05
E-t0m <110770475+E-t0m@users.noreply.github.com>
2026-05-14 11:44:09 +0200

    Add files via upload

---
+++ block-monitor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from ve_aggregator import VEDirect
+from time import sleep
+from datetime import datetime
+
+PORT = '/dev/ttyACM3'
+BAUD = 19200
+
+def fmt(ts):
+	return datetime.fromtimestamp(ts).strftime('%H:%M:%S.%f')[:-3]
+
+with VEDirect(PORT, BAUD) as vd:
+	last_ts   = {}
+	prev_time = {}   # {ser: previous arrival time}
+	
+	while True:
+		sleep(0.05)
+		data = vd.get_all()
+		if not data: continue
+		
+		for k, d in data.items():
+			if d['ts'] == last_ts.get(k): continue
+			last_ts[k] = d['ts']
+			ser = d.get('SER#', k)
+			now = d['ts']
+			
+			interval = ''
+			if ser in prev_time:
+				interval = f'  {(now - prev_time[ser]) * 1000:.0f}ms'
+			prev_time[ser] = now
+			
+			print(f'{fmt(now)}  {ser}{interval}', flush=True)
